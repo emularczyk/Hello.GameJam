86e210bda64878c3f132a992f920bc5a1e0cea5a
Amaimon208 <amaimon208@o2.pl>
2022-04-22 17:35:20 +0200

    Fix

---
+++ Assets/Scripts/EnemyMoves.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoves : MonoBehaviour
+{
+    [SerializeField] private float ourY;
+    [SerializeField] private float freguency;
+    [SerializeField] private float amplitude;
+    [SerializeField] private bool inverted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ourY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+        float sin = Mathf.Sin(pos.x * freguency) * amplitude;
+        pos.y = ourY + sin;
+        if (inverted)
+            pos *= -1;
+        transform.position = pos;
+    }
+}
