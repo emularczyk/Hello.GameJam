2eed0be37a01c43109268b155be47b9cfe28a41a
Amaimon208 <amaimon208@o2.pl>
2022-04-22 19:58:30 +0200

    MoveSin

--- Assets/Scripts/EnemyMoves.cs
+++ Assets/Scripts/EnemyMoves.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMoves : MonoBehaviour
 {
     private float ourY;
+    private float ourX;
     [SerializeField] private float freguency;
     [SerializeField] private float amplitude;
     [SerializeField] private bool inverted;
@@ -13,20 +14,30 @@ public class EnemyMoves : MonoBehaviour
     void Start()
     {
         ourY = transform.position.y;
+        ourX = transform.position.x;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        MoveSin();
+        MoveSiny();
     }
-    public void MoveSin()
+    public void MoveSinx()
     {
         Vector2 pos = transform.position;
         float sin = Mathf.Sin(pos.x * freguency) * amplitude;
         pos.y = ourY + sin;
         if (inverted)
-            pos *= -1;
+            sin *= -1;
+        transform.position = pos;
+    }
+    public void MoveSiny()
+    {
+        Vector2 pos = transform.position;
+        float sin = Mathf.Sin(pos.y * freguency) * amplitude;
+        pos.x = ourX + sin;
+        if (inverted)
+            sin *= -1;
         transform.position = pos;
     }
 }
--- Assets/Scripts/GhostEnemy.cs
+++ Assets/Scripts/GhostEnemy.cs
@@ -7,6 +7,12 @@ public class GhostEnemy : Enemy
     public Transform player;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Vector2 direction;
+    private float ourY;
+    private float ourX;
+    [SerializeField] private float freguency;
+    [SerializeField] private float amplitude;
+    [SerializeField] private bool inverted;
 
     // Start is called before the first frame update
     void Awake()
@@ -16,34 +22,51 @@ public class GhostEnemy : Enemy
             player = GameObject.Find("Player").GetComponent<Transform>();
         }
         rb = this.GetComponent<Rigidbody2D>();
+        ourY = transform.position.y;
+        ourX = transform.position.x;
     }
 
-    
+
     // Update is called once per frame
     void Update()
     {
-        if (player != null)
-        {  // Following Player
-       Vector3 direction = player.position - transform.position;
-       float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-       direction.Normalize();
-       movement = direction;
-        }
     }
-    
+
     private void FixedUpdate()
     {
-        moveCharacter(movement);
+        attack();
     }
 
-    void moveCharacter(Vector2 direction)
+    void attack()
     {
-        /* Vector2 pos = transform.position;
-         pos.x -= speed * Time.fixedDeltaTime;
-         transform.position = pos;*/
-
-        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.fixedDeltaTime));
+        lokingForPlayer();
+        rb.MovePosition((Vector2)transform.position + ( direction * speed * Time.fixedDeltaTime));
         
     }
-
+    public void lokingForPlayer()
+    {
+        if (player != null)
+        {  // Following Player
+            direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            direction.Normalize();
+            movement = direction;
+        }
+    }
+    void moveDown()
+    {
+        Vector2 pos = transform.position;
+        pos.y -= speed * Time.fixedDeltaTime;
+        transform.position = pos;
+    }
+    void moveDownSin()//freguency=1,amplitude=50
+    {
+        Vector2 pos = transform.position;
+        float sin = Mathf.Sin(pos.y* freguency)*amplitude;
+        if (inverted)
+            sin *= -1;
+        pos.x = ourX+sin* Time.fixedDeltaTime;
+        pos.y -= speed * Time.fixedDeltaTime;
+        transform.position = pos;
+    }
 }
