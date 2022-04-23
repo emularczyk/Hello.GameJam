4edff7e8d9cdb6981ca62d91ee0ad385e5abd70e
emularczyk <emilia.mularczyk00@gmail.com>
2022-04-23 19:06:03 +0200

    fix

--- Assets/Scripts/RainbowShard.cs
+++ Assets/Scripts/RainbowShard.cs
@@ -2,13 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RainbowShard : MonoBehaviour
+public class RainbowShardScript : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Destroy(this.gameObject);
-        }
-    }
+
 }
