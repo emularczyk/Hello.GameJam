36bd792e79917db1cc85b8c2730ad5506697e527
Amaimon208 <amaimon208@o2.pl>
2022-04-23 10:13:26 +0200

    Time

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,14 +18,8 @@ public class Player : MonoBehaviour
 
     // Attack
     [SerializeField] private Bullet[] attackTypes;
-    private int rainbowFragments = 0;
+    [SerializeField] private int rainbowFragments = 0;
     int attackNumber = 0;
-    /*syf do ataku
-    DateTime start;
-    DateTime end;
-    TimeSpan t;
-    int diff;
-    */
 
     private void Start()
     {
@@ -35,7 +29,6 @@ public class Player : MonoBehaviour
     private void Update()
     {
         movement();
-        // start = DateTime.Now; //syf do ataku
         if (Input.GetKeyDown(KeyCode.Space))
         {
             attackNumber++;
@@ -49,43 +42,6 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(attackTypes[attackNumber], transform.position, Quaternion.identity);
-
-            /* syf do ataku
-            print(start);
-            end = DateTime.Now;
-            print(end);
-            diff = (int)end.Subtract(start).TotalSeconds;
-            print(t);
-            print(diff);
-            if (rainbowFragments == 0)
-            {
-                Instantiate(attackTypes[0], transform.position, Quaternion.identity);
-            }
-            else if (rainbowFragments > 0 && diff < 1)
-            {
-                Instantiate(attackTypes[0], transform.position, Quaternion.identity);
-            }
-            else if(rainbowFragments >= 1 && diff < 2)
-            {
-                Instantiate(attackTypes[1], transform.position, Quaternion.identity);
-            }
-            else if (rainbowFragments >= 2 && diff < 3)
-            {
-                Instantiate(attackTypes[2], transform.position, Quaternion.identity);
-            }
-            else if (rainbowFragments >= 3 && diff < 4)
-            {
-                Instantiate(attackTypes[3], transform.position, Quaternion.identity);
-            }
-            else if (rainbowFragments >= 4 && diff < 5)
-            {
-                Instantiate(attackTypes[4], transform.position, Quaternion.identity);
-            }
-            else if (rainbowFragments >= 5 && diff < 6)
-            {
-                Instantiate(attackTypes[5], transform.position, Quaternion.identity);
-            }
-            */
         }
     }
 
