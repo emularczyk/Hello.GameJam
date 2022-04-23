1573aa22eeb5131d5612a9a081ee371ffba77665
Amaimon208 <amaimon208@o2.pl>
2022-04-23 19:39:05 +0200

    BackOnATrack

--- Assets/Scripts/RainbowShard.cs
+++ Assets/Scripts/RainbowShard.cs
@@ -2,7 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RainbowShardScript : MonoBehaviour
+public class RainbowShard : MonoBehaviour
 {
-
+    [SerializeField]  private int speed=1;
+    [SerializeField] private Spawn spawn;
+    void Start()
+    {
+        spawn= GameObject.Find("GameManager").GetComponent<Spawn>();
+    }
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        transform.Translate(Vector2.down *speed* Time.deltaTime / Time.timeScale);
+        if (transform.position.y <1)
+            speed = 0;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            spawn.isReady = true;
+            Destroy(this.gameObject);
+        }
+    }
 }
